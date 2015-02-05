ef2b42ba57f67428149bbafaf096fa74d346666d
3rc <thedj3rc@gmail.com>
2015-02-04 17:11:46 -0700

    Quotes almost done

--- quotes/main.js
+++ quotes/main.js
@@ -4,30 +4,66 @@ $(document).on('ready', function() {
 		this.quoteText = quoteText;
 		this.author = author;
 		this.rating = rating;
-		this.quoteId = _.uniqueId('quote')
 	}
 
-	var quote1 = new Quote("Dude, sucking at something is the first step to being sorta good at something",
-		"Jake the Dog",
-		5)
+	Quote.prototype.idUnique = _.uniqueId('');
+	Quote.prototype.Constructor = Quote;
 
-	var deleteQuote = "<button class='deleteQuote'>Delete</button>"
-	$("table").append("<tr><td id='quoteItemText'>" + quote1.quoteText + "</td><td id='quoteItemAuthor'>" + quote1.author + "</td><td id='quoteItemRating'>" + quote1.rating + "</td><td id='quoteItemDelete'>" + deleteQuote + "<td></tr>" )
+	var allQuotes = [];
+
+	var deleteQuote = "<button class='deleteQuote'>Delete</button>";
 
 	$(".quoteSubmit").click(function() {
 		var quoteInput = $("#quoteForm").val();
 		var authorInput = $("#authorForm").val();
 		var ratingInput = $("#ratingForm").val();
+		if(quoteInput == "" || authorInput == "" || ratingInput == "") {
+			alert("Please complete the quote!")
+		}
 
-		$("table").append("<tr><td id='quoteItemText'>" + quoteInput + "</td><td id='quoteItemAuthor'>" + authorInput + "</td><td id='quoteItemRating'>" + ratingInput + "</td><td id='quoteItemDelete'>" + deleteQuote + "</td></tr>" )
+		else if( isNaN(ratingInput) || ratingInput > 5){
+			alert("Please rate your quote 1-5 stars!")
+		}
 
+		else {
+			$("input").val("");
+
+			allQuotes.push(new Quote(quoteInput, authorInput, ratingInput));
+
+			$(".quoteItems").remove();
+
+			var sortedQuotes = _.sortBy(allQuotes, function(allQuotes) {return (allQuotes.rating)}).reverse();
+
+			var filteredSortedQuotes = _.filter(sortedQuotes, function(sortedQuotes) {return (sortedQuotes.rating !== null)});
+
+			for(i = 0; i < filteredSortedQuotes.length; i++) {
+				$("table").append("<tr class='quoteItems'><td id='quoteItemText'>" + 
+					filteredSortedQuotes[i].quoteText + 
+					"</td><td id='quoteItemAuthor'>" + 
+					filteredSortedQuotes[i].author + 
+					"</td><td id='quoteItemRating'>" + 
+					filteredSortedQuotes[i].rating + 
+					"★</td><td id='" + sortedQuotes[i].idUnique + "'>" + 
+					deleteQuote + "</td></tr>" )
+			}
+		}
 		$(".deleteQuote").click(function() {
-		$(this).parent().parent().remove();
-	});
+			$(this).parent().parent().remove();
+			allQuotes[$(this).parent().attr("id") - 1].quoteText = null;
+			allQuotes[$(this).parent().attr("id") - 1].author = null;
+			allQuotes[$(this).parent().attr("id") - 1].rating = null;
+
+		});
 	});
 
-	$(".deleteQuote").click(function() {
-		$(this).parent().parent().remove();
-	});
+
+	$("#formHide").click(function() {
+		$(".quoteForm").hide();
+		$("body").append("<a href='#' id='formShow'>Click to Add More Quotes!</a>");
+		$("#formShow").click(function() {
+			$(".quoteForm").show();
+			$(this).remove();
+		})
+	})
   
 });
